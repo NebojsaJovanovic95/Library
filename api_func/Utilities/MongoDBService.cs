b71b65ec938d92fbd81ddee899092b5351c26379
Jovanovic <nebojsa.jovanovic@hatch.com>
2024-01-03 15:28:14 -0500

    new year :)

--- api_func/Utilities/MongoDBService.cs
+++ api_func/Utilities/MongoDBService.cs
@@ -2,23 +2,83 @@
 
 using MongoDB.Driver;
 using Library;
+using Amazon.SecurityToken.Model;
+using MongoDB.Bson;
 public class MongoDBService
 {
-    private readonly IMongoCollection<Book> _booksCollection;
-    private readonly IMongoCollection<User> _usersCollection;
+    private static MongoClient _client;
 
-    public MongoDBService(string databaseName) {
-        var mongoDBConnetionString = Environment
-        .GetEnvironmentVariable(
-            "MongoDBConnectionString"
-        );
-        var mongoClient = new MongoClient(
-            mongoDBConnetionString
-        );
-        var database = mongoClient.GetDatabase(databaseName);
-        _booksCollection = database.GetCollection<Book>("Books");
-        _usersCollection = database.GetCollection<User>("Users");
+    public MongoDBService() {
+        _client = new MongoClient(
+            Environment.GetEnvironmentVariable(
+                "MongoDBConnectionString"
+            )
+        );  
     }
+
+    public List<Book> getAllBooks(
+
+    ) {
+        return _client
+        .GetDatabase("library")
+        .GetCollection<Book>("books")
+        .Find(
+            new BsonDocument()
+        ).ToList();
+    }
+
+    public List<User> getAllUsers(
+
+    ) {
+        return _client
+        .GetDatabase("library")
+        .GetCollection<User>("users")
+        .Find(
+            new BsonDocument()
+        ).ToList();
+    }
+    
+    public User getUserById(
+        string userID
+    ) {
+        return _client
+        .GetDatabase("library")
+        .GetCollection<User>("users")
+        .Find(
+            new BsonDocument(
+                // write filter string for userID
+            )
+        ).First<User>();
+    }
+
+    private string collectionFromType(Object object) {
+        // figure out collection by type
+        return "users";
+    }
+
+    public List<T> getAll(string filter) {
+        return _client
+        .GetDatabase("library")
+        .GetCollection<T>(collectionFromType(new T()))
+        .Find(
+            new BsonDocument()
+        ).ToList();
+    }
+    // private readonly IMongoCollection<Book> _booksCollection;
+    // private readonly IMongoCollection<User> _usersCollection;
+
+    // public MongoDBService(string databaseName) {
+    //     var mongoDBConnetionString = Environment
+    //     .GetEnvironmentVariable(
+    //         "MongoDBConnectionString"
+    //     );
+    //     var mongoClient = new MongoClient(
+    //         mongoDBConnetionString
+    //     );
+    //     var database = mongoClient.GetDatabase(databaseName);
+    //     _booksCollection = database.GetCollection<Book>("Books");
+    //     _usersCollection = database.GetCollection<User>("Users");
+    // }
 
     public List<Book> GetAllBooks()
     {
